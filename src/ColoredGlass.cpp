8da8966887b676e9679ee97c6d5f3f7d33c9e67a
Alexey Komarov <alexey@komarov.org.ru>
2022-05-09 09:00:18 +0400

    Adjust colors

--- src/ColoredGlass.cpp
+++ src/ColoredGlass.cpp
@@ -154,9 +154,10 @@ void setColors(int shift) {
 		TParticle &p = Particles[i];
 		double angle = p.vector + shift / 100;
 
-		p.red = getSin(angle / 3) * 127 + 128;
-		p.green = getSin(angle / 4) * 127 + 128;
-		p.blue = getSin(angle / 12) * 127 + 128;
+		p.red = getSin(angle / 3) * 255;
+		printf("%d\n", p.red);
+		p.green = getCos(angle / 3) * 255;
+		p.blue = getSin(angle / 5) * 255;
 	}
 }
 
@@ -263,9 +264,9 @@ struct ColoredGlassGlWidget : ModuleLightWidget {
 			drawParticle(args, &Particles[i]);
 			TParticle p = Particles[i];
 
-			int red = int(p.red * Settings.red / 5) % 127 + 128;
-			int green = int(p.green * Settings.green / 5) % 127 + 128;
-			int blue = int(p.blue * Settings.blue / 5) % 127 + 128;
+			int red = int(p.red * Settings.red / 10);
+			int green = int(p.green * Settings.green / 10);
+			int blue = int(p.blue * Settings.blue / 10);
 
 			nvgFillColor(args.vg, nvgRGBA(red, green, blue,
 				int(Settings.alpha + p.alpha * Settings.alphaRand)));
